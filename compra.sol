a0dee4190ac1140273a277cc5dd41700b2aec005
Lucas Moreira Minchillo <64343934+minchillo4@users.noreply.github.com>
2021-10-19 22:19:40 -0300

    Update compra.sol

--- compra.sol
+++ compra.sol
@@ -3,27 +3,65 @@ pragma solidity 0.8.9;
 
 contract compraEVenda {
     
+    
+    
     string comprador;
     string vendedor;
     string matricula;
     string cartorio;
     string dataDeVencimento;
+    
     bool quitado = false;
-    uint public valorTotal = 100000;
-    uint public valorDaParcela;
+    
+    uint public valorTotal;
     uint public valorEntrada;
     uint public valorEmAberto;
+    uint public valorDaParcela;
     uint quantidadeDeParcelas;
     uint porcentagemMulta;
     
-    function pagarEntrada(uint _valorPagamento) public returns(string memory, uint) {
-            valorEntrada = _valorPagamento;
-            valorEmAberto = valorTotal - _valorPagamento;
-            return ("Valor em Aberto: ", valorEmAberto);
+    constructor(
+        uint _valorTotal,
+        uint _valorEntrada,
+        uint _quantidadeDeParcelas,
+        string memory _matricula,
+        string memory _cartorio
+        ) 
+    {
+        valorEntrada = _valorEntrada;    
+        valorTotal = _valorTotal;
+        quantidadeDeParcelas = _quantidadeDeParcelas;
+        matricula =  _matricula;
+        cartorio =  _cartorio;
+        valorDaParcela = valorParcela();
+        
     }
     
     
     
     
+    function pagarEntrada(uint _valorPagamento) public returns(string memory, uint) {
+        valorEntrada = _valorPagamento;
+        valorEmAberto = valorTotal - _valorPagamento;
+        return ("Valor em Aberto: ", valorEmAberto);
+    }
     
+    function pagarParcela(uint _valorDaParcela) public returns (uint, string memory)  {
+        valorEmAberto -= _valorDaParcela;
+        return(valorEmAberto, "valor em aberto: ");
+    }
+    
+    function valorParcela() public view returns (uint) {
+        uint calculoValorParcela = (valorTotal - valorEntrada) / quantidadeDeParcelas;
+        return(calculoValorParcela);
+    }
+
+    function valorMulta (uint _percentualMulta) public view returns (uint, string memory) {
+        uint multa = (_percentualMulta/100) * valorTotal;
+        return(multa, "sua multa");
+    }
+
 }
+
+
+
